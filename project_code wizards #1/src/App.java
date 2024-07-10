90f4588fcf10d2f316c0ed24af95ae76cc75c1d9
trvpboi50 <denis.kasolo@studmc.kiu.ac.ug>
2024-07-10 05:55:08 +0000

    Pending changes exported from your codespace

---
+++ project_code wizards #1/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class App {
+    static Scanner input=new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
